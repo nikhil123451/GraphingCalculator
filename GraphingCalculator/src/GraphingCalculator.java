c6442b1e6b3a2c63aec221ee1ede9e1ec0f2c6bf
38262006 <38262006@L829WD210026.edu.cbe.ab.ca>
2026-05-13 13:59:02 -0600

    made Jframe window (blank rn)

--- GraphingCalculator/src/GraphingCalculator.java
+++ GraphingCalculator/src/GraphingCalculator.java
@@ -1,5 +1,15 @@
 import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 public class GraphingCalculator {
-	
+	public static void main(String[] args) {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int screenWidth = (int) screenSize.getWidth();
+		int screenHeight = (int) screenSize.getHeight();
+		
+		JFrame frame = new JFrame("Graphing Calculator");
+		frame.setVisible(true);
+		frame.setBounds(screenHeight/2, screenWidth/8, 960, 600); //found these create the ideal window
+	}
 }
