ff453fdc595f15d9bee64c138d6da48263815835
nikti <nikti@Andy>
2026-05-21 14:34:30 -0600

    made constructors more streamlined

--- GraphingCalculator/src/Line.java
+++ GraphingCalculator/src/Line.java
@@ -7,12 +7,17 @@ public class Line {
 	protected int y2;
 	protected Color color;
 	protected int stroke;
+	static final int DEFAULT_STROKE = 4;
 	
 	public Line(int x1, int y1, int x2, int y2) {
-		this(x1, y1, x2, y2, Color.BLACK, 4);
+		this(x1, y1, x2, y2, DEFAULT_STROKE, Color.BLACK);
 	}
 	
-	public Line(int x1, int y1, int x2, int y2, Color color, int stroke) {
+	public Line(int x1, int y1, int x2, int y2, int stroke) {
+		this(x1, y1, x2, y2, stroke, Color.BLACK);
+	}
+	
+	public Line(int x1, int y1, int x2, int y2, int stroke, Color color) {
 		this.x1 = x1;
 		this.y1 = y1;
 		this.x2 = x2;
